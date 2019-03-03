43c938003b50867ddee060a77ae8a3cfe01ae751
Dmitry <omigoneo@gmail.com>
2019-03-03 20:17:48 +0300

    feat(Category map): added Category map

---
+++ src/Lib/Confetti.Infrastructure.Data/Mapping/Catalog/CategoryMap.cs
@@ -0,0 +1,37 @@
+using Confetti.Domain.Core.Catalog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confetti.Infrastructure.Data.Mapping.Catalog
+{
+    /// <summary>
+    /// Represents a category mapping configuration
+    /// </summary>
+    public partial class CategoryMap : ConfettiEntityTypeConfiguration<Category>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configures the entity
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity</param>
+        public override void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable(nameof(Category));
+            builder.HasKey(category => category.Id);
+
+            builder.Property(category => category.Code).HasMaxLength(100).IsRequired();
+            builder.Property(category => category.Title).HasMaxLength(400).IsRequired();
+            builder.Property(category => category.MetaKeywords).HasMaxLength(400);
+            builder.Property(category => category.MetaTitle).HasMaxLength(400);
+
+            // Indexes
+            builder.HasIndex(c => c.ParentId);
+            builder.HasIndex(c => c.Position);
+
+            base.Configure(builder);
+        }
+
+        #endregion
+    }
+}
