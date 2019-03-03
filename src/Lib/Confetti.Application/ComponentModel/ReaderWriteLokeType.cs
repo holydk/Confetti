845baead68708b1229146f02c0ba80f02c7a2681
Dmitry <omigoneo@gmail.com>
2019-03-03 20:20:43 +0300

    feat(server (web api) caching): added base server caching in web api project

---
+++ src/Lib/Confetti.Application/ComponentModel/ReaderWriteLokeType.cs
@@ -0,0 +1,12 @@
+namespace Confetti.Application.ComponentModel
+{
+    /// <summary>
+    /// Reader/Write locker type
+    /// </summary>
+    public enum ReaderWriteLockType
+    {
+        Read,
+        Write,
+        UpgradeableRead
+    }
+}
