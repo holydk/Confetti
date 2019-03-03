c5f6bfba4748a87cabe6177c87794ea1da0e3141
Dmitry <omigoneo@gmail.com>
2019-03-03 20:26:00 +0300

    feat(web-api models map): added models mapping in web-api

---
+++ src/Lib/Confetti.Application/Mapping/IOrderedMapperProfile.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Confetti.Application.Mapping
+{
+    /// <summary>
+    /// Mapper profile registrar interface
+    /// </summary>
+    public interface IOrderedMapperProfile
+    {
+        /// <summary>
+        /// Gets order of this configuration implementation
+        /// </summary>
+        int Order { get; }
+
+        /// <summary>
+        /// Configure profile
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        void Configure(IServiceCollection services);
+    }
+}
