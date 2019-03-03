0192ddfaf7372d897904908a268ee6cdb9428b13
Dmitry <omigoneo@gmail.com>
2019-03-03 20:26:47 +0300

    feat(web-api category map): added category map in web-api

---
+++ src/Lib/Confetti.Application/Models/Catalog/CategoryModel.cs
@@ -0,0 +1,30 @@
+namespace Confetti.Application.Models.Catalog
+{
+    public partial class CategoryModel
+    {
+        /// <summary>
+        /// Gets or sets the title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the meta title
+        /// </summary>
+        public string MetaTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the meta description
+        /// </summary>
+        public string MetaDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets the meta keywords
+        /// </summary>
+        public string MetaKeywords { get; set; }
+    }
+}
