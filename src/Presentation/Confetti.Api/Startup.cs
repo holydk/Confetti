6d4436460dd330cd0f9352d2648104c0d98f0bf2
Dmitry <omigoneo@gmail.com>
2019-04-05 21:48:07 +0300

    feat(web-api base config): added startup config and connection string

--- src/Presentation/Confetti.Api/Startup.cs
+++ src/Presentation/Confetti.Api/Startup.cs
@@ -1,25 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using Confetti.Api.Areas.Public.Factories;
+using Confetti.Application.Caching;
+using Confetti.Application.Catalog;
+using Confetti.Application.Installation;
+using Confetti.Application.Mapping;
+using Confetti.Domain.Interfaces;
+using Confetti.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace Confetti.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services
                 .AddMvcCore()
-                .AddJsonFormatters();
+                .AddJsonFormatters(settings =>
+                {
+                    settings.Formatting = Newtonsoft.Json.Formatting.None;
+                    settings.NullValueHandling = NullValueHandling.Ignore;
+                });
 
             services.AddCors();
+            
+            var migrationsAssembly = typeof(ConfettiContext).Assembly.GetName().Name;
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DbContext, ConfettiContext>(options =>
+                options.UseSqlServer(connectionString,
+                x => x.MigrationsAssembly(migrationsAssembly)));
+
+            // dynamically load all entity and query type configurations
+            var typeConfigurations = typeof(IOrderedMapperProfile).Assembly.GetTypes().Where(type =>
+                (type.GetInterface(typeof(IOrderedMapperProfile).Name) == typeof(IOrderedMapperProfile)) 
+                  && (!type.IsAbstract));
+
+            services.AddAutoMapper(c => {
+                foreach (var typeConfiguration in typeConfigurations)
+                {
+                    var configuration = (IOrderedMapperProfile)Activator.CreateInstance(typeConfiguration);
+                    configuration.Configure(services);
+
+                    c.AddProfile(typeConfiguration);
+                }
+            });
+
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddScoped<ICacheManager, PerRequestCacheManager>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IInstallationService, CodeFirstInstallationService>();
+
+            services.AddSingleton<IStaticCacheManager, MemoryCacheManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +95,19 @@ namespace Confetti.Api
             });
 
             app.UseMvc();
+
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                // Make sure we have the database
+                serviceScope.ServiceProvider
+                    .GetService<DbContext>().Database.Migrate();
+                serviceScope.ServiceProvider
+                    .GetService<IInstallationService>()
+                    .InstallDataAsync(updateData: false)
+                    .GetAwaiter()
+                    .GetResult();
+            }
         }
     }
 }
