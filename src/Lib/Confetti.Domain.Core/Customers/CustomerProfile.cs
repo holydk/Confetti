18044a074b089e36cf5028bdcf308f97f48fd30b
Dmitry <omigoneo@gmail.com>
2019-04-06 15:18:37 +0300

    feat(web-api customer model): added customer model, map

---
+++ src/Lib/Confetti.Domain.Core/Customers/CustomerProfile.cs
@@ -0,0 +1,14 @@
+namespace Confetti.Domain.Core.Customers
+{
+    /// <summary>
+    /// Represents a customer profile
+    /// </summary>
+    public partial class CustomerProfile
+    {
+        /// <summary>
+        /// Gets or sets the customer id
+        /// </summary>
+        /// <value>Subject guid</value>
+        public string SubjectId { get; set; }
+    }
+}
---
+++ src/Lib/Confetti.Infrastructure.Data/Mapping/Customers/CustomerProfileMap.cs
@@ -0,0 +1,28 @@
+using Confetti.Domain.Core.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confetti.Infrastructure.Data.Mapping.Customers
+{
+    /// <summary>
+    /// Represents a customer profile mapping configuration
+    /// </summary>
+    public partial class CustomerProfileMap : ConfettiEntityTypeConfiguration<CustomerProfile>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configures the entity
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity</param>
+        public override void Configure(EntityTypeBuilder<CustomerProfile> builder)
+        {
+            builder.ToTable(nameof(CustomerProfile));
+            builder.HasKey(profile => profile.SubjectId);
+
+            base.Configure(builder);
+        }
+            
+        #endregion
+    }
+}
