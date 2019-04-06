2ac83c9299da816c529fb9f755b1e6c6319e3a8f
Dmitry <omigoneo@gmail.com>
2019-04-06 15:34:35 +0300

    feat(web-api back in stock subscription model): added back in stock subscription model, map

---
+++ src/Lib/Confetti.Domain.Core/Catalog/BackInStockSubscription.cs
@@ -0,0 +1,36 @@
+using System;
+using Confetti.Domain.Core.Customers;
+
+namespace Confetti.Domain.Core.Catalog
+{
+    /// <summary>
+    /// Represents a back in stock subscription
+    /// </summary>
+    public partial class BackInStockSubscription
+    {
+        /// <summary>
+        /// Gets or sets the product identifier
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the customer profile identifier
+        /// </summary>
+        public string CustomerProfileId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of instance creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets the product
+        /// </summary>
+        public virtual Product Product { get; set; }
+
+        /// <summary>
+        /// Gets the customer profile
+        /// </summary>
+        public virtual CustomerProfile CustomerProfile { get; set; }
+    }
+}
---
+++ src/Lib/Confetti.Infrastructure.Data/Mapping/Catalog/BackInStockSubscriptionMap.cs
@@ -0,0 +1,47 @@
+using Confetti.Domain.Core.Catalog;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Confetti.Infrastructure.Data.Mapping.Catalog
+{
+    /// <summary>
+    /// Represents a back in stock subscription mapping configuration
+    /// </summary>
+    public partial class BackInStockSubscriptionMap : ConfettiEntityTypeConfiguration<BackInStockSubscription>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configures the entity
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity</param>
+        public override void Configure(EntityTypeBuilder<BackInStockSubscription> builder)
+        {
+            builder.ToTable(nameof(BackInStockSubscription));
+            builder.HasKey(subscription => new 
+            {
+                subscription.ProductId,
+                subscription.CustomerProfileId
+            });
+
+            // Relations
+            builder.HasOne(subscription => subscription.Product)
+                .WithMany()
+                .HasForeignKey(subscription => subscription.ProductId)
+                .IsRequired();
+
+            builder.HasOne(subscription => subscription.CustomerProfile)
+                .WithMany()
+                .HasForeignKey(subscription => subscription.CustomerProfileId)
+                .IsRequired();
+
+            // Indexes
+            builder.HasIndex(subscription => subscription.ProductId);
+            builder.HasIndex(subscription => subscription.CustomerProfileId);
+
+            base.Configure(builder);
+        }
+
+        #endregion
+    }
+}
