fa5a1835bd776434f806f4d50fecccc305e6d9a6
Dmitry Kiselev <omigoneo@gmail.com>
2019-09-28 00:32:18 +0300

    feat: add ef core sql server data provider

---
+++ src/Lib/Confetti.Infrastructure.EntityFrameworkCore.SqlServer/src/SqlServerDataProvider.cs
@@ -0,0 +1,44 @@
+using System.Data.Common;
+using System.Data.SqlClient;
+using Confetti.Data;
+
+namespace Confetti.Infrastructure.EntityFrameworkCore.SqlServer
+{
+    /// <summary>
+    /// Represents SQL Server data provider
+    /// </summary>
+    public abstract class SqlServerDataProvider : IDataProvider
+    {
+        #region Methods
+
+        /// <summary>
+        /// Initialize database
+        /// </summary>
+        public abstract void InitializeDatabase();
+
+        /// <summary>
+        /// Get a support database parameter object (used by stored procedures)
+        /// </summary>
+        /// <returns>Parameter</returns>
+        public virtual DbParameter GetParameter()
+        {
+            return new SqlParameter();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether this data provider supports backup
+        /// </summary>
+        public virtual bool BackupSupported => true;
+
+        /// <summary>
+        /// Gets a maximum length of the data for HASHBYTES functions, returns 0 if HASHBYTES function is not supported
+        /// </summary>
+        public virtual int SupportedLengthOfBinaryHash => 8000; // for SQL Server 2008 and above HASHBYTES function has a limit of 8000 characters.
+
+        #endregion
+    }
+}
